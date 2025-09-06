341c472e356c6ca31dc3b404f1ce1c2a9adeb993
hostefrain <hostefrain@gmail.com>
2025-09-06 01:25:05 -0300

    arreglo-main

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Habilita CORS para el frontend
+  app.enableCors({
+    origin: 'https://2025-proyecto1-front-imc-phi.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }); // Habilita CORS para el frontend
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.init();
   return app;
